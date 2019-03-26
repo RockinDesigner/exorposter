054f8168987d0103b31cd441130720aa9f6409db
RockinDesigner <43171022+RockinDesigner@users.noreply.github.com>
2019-03-26 09:40:04 +0300

    desktop

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,8 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 
 gulp.task('scripts', function() {
     return gulp.src([ // Берем все необходимые библиотеки
-        'app/libs/jquery/dist/jquery.min.js', // Берем jQuery
+        'app/libs/jquery/dist/jquery.min.js',
+        'app/libs/mgglitch/mgGlitch.min.js'// Берем jQuery
     ])
         .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
         .pipe(uglify()) // Сжимаем JS файл
